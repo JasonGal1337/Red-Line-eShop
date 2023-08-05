302b8c1073ef0bd669e111c3e17e7bf902e85c36
JasonGal <jason.gal.bu@gmail.com>
2023-08-05 13:18:08 +0300

    edited product controller to handle cloudinary uploads

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -10,20 +10,103 @@ const getOneProduct = async (req,res) => {
     res.send(product);
 };
 
-const postOneProduct = async (req,res) => {
-    const newProduct = await Product.create(req.body);
-    res.send({ msg: "product logged successfully" });
-};
+const postOneProduct = async (req, res) => {
+    uploadProductImages(req, res, async (err) => {
+      if (err) {
+        console.log('Error uploading files: ', err);
+        return res.status(500).json({ error: 'File upload failed' });
+      }
+  
+      try {
+        const { title, description, price, technicalInformation, stockQuantity } = req.body;
+        const images = req.files;
+  
+        if (!title || !description || !price || !technicalInformation || !stockQuantity || images.length === 0) {
+          console.log('Error missing required fields or images');
+          return res.status(400).json({ error: 'Missing required fields or images' });
+        }
+  
+        const uploadPromises = images.map((image) => cloudinary.uploader.upload(image.path, { folder: 'Products' }));
+        const uploadedImages = await Promise.all(uploadPromises);
+  
+        const imageObjects = uploadedImages.map((result) => ({
+          public_id: result.public_id,
+          url: result.secure_url,
+        }));
+  
+        const newProduct = await Product.create({
+          title,
+          description,
+          price,
+          technicalInformation,
+          stockQuantity,
+          images: imageObjects,
+        });
+  
+        res.status(201).json({ msg: 'Product created successfully', product: newProduct });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+      }
+    });
+  };
 
 const deleteProduct = async (req,res) => {
     const deletedProduct = await Product.deleteOne({ _id: req.params.id });
     res.send({ msg: "product deleted" });
 };
 
-const updateProduct = async (req,res) => {
-    const updatedProduct = await Product.findByIdAndUpdate({ _id: req.params.id }, req.body);
-    res.send({ msg: "product updated "});
-};
+const updateProduct = async (req, res) => {
+    uploadProductImages(req, res, async (err) => {
+      if (err) {
+        console.log('Error uploading files: ', err);
+        return res.status(500).json({ error: 'File upload failed' });
+      }
+  
+      try {
+        const { title, description, price, technicalInformation, stockQuantity } = req.body;
+        const images = req.files;
+  
+        if (!title || !description || !price || !technicalInformation || !stockQuantity) {
+          console.log('Error missing required fields');
+          return res.status(400).json({ error: 'Missing required fields' });
+        }
+  
+        let updatedProductData = {
+          title,
+          description,
+          price,
+          technicalInformation,
+          stockQuantity,
+        };
+  
+        // Check if new images were uploaded
+        if (images.length > 0) {
+          const uploadPromises = images.map((image) => cloudinary.uploader.upload(image.path, { folder: 'Products' }));
+          const uploadedImages = await Promise.all(uploadPromises);
+  
+          const imageObjects = uploadedImages.map((result) => ({
+            public_id: result.public_id,
+            url: result.secure_url,
+          }));
+  
+          updatedProductData.images = imageObjects;
+        }
+  
+        // Update the product in the database
+        const updatedProduct = await Product.findByIdAndUpdate(
+          { _id: req.params.id },
+          updatedProductData,
+          { new: true } // Return the updated product object
+        );
+  
+        res.status(200).json({ msg: 'Product updated successfully', product: updatedProduct });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+      }
+    });
+  };
 
 const getAllUserProducts = async (req,res) => {
     const userProducts = await Product.find({ userId: req.params.userId });
